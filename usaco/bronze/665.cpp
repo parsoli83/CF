a7d14c60a9a29203c0a44628bcc0b7957e4f53a0
parsoli83 <mr.parsoli@gmail.com>
2024-01-14 11:35:23 +0330

    solved two medium problems 616 664

--- usaco/bronze/665.cpp
+++ usaco/bronze/665.cpp
@@ -6,9 +6,29 @@ using namespace std;
 
 int main()
 {
-    freopen("mixmilk.in", "r", stdin);
-    
-    freopen("mixmilk.out", "w", stdout);
+    int h,w,k;
+    freopen("cowsignal.in", "r", stdin);
+    scanf("%d %d %d",&h,&w,&k);
+    string moo[h];
+    for (int i=0;i<h;i++)
+    {
+        cin >> moo[i];
+    }
+    freopen("cowsignal.out", "w", stdout);
+    for (int i=0;i<h;i++)
+    {
+        for (int h_k=0;h_k<k;h_k++)
+        {
+            for (int j=0;j<w;j++)
+            {
+                for (int w_k=0; w_k<k;w_k++)
+                {
+                    cout << moo[i][j];
+                }
+            }
+            printf("\n");
+        }
+    }
     
     return 0;
 }
